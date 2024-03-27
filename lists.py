6d216bbf3239a2d67e9635422c990b6c2e63c1d5
Miika Haapanen <miika.haapanen@edu.lapinamk.fi>
2024-03-27 18:00:55 +0200

    Lista sensor_id stä, jotka liittyy kwh consumption ja production

---
+++ lists.py
@@ -0,0 +1,76 @@
+# tasainen arvo
+lights_ids = [
+    "68_50_1_Value_65537",
+    "68_50_2_Value_65537",
+    "71_50_1_Value_65537",
+    "71_50_2_Value_65537",
+    "103_50_1_Value_65537",
+    "106_50_1_Value_65537",
+    "110_50_1_Value_65537",
+    "112_50_1_Value_65537",
+    "116_50_1_Value_65537",
+    "120_50_1_Value_65537",
+    "122_50_1_Value_65537",
+    "122_50_2_Value_65537",
+    "141_50_1_Value_65537",
+    "141_50_2_Value_65537",
+    "142_50_1_Value_65537",
+    "142_50_2_Value_65537"
+]
+
+# tasainen arvo
+outlet_ids = [
+    "148_50_0_Value_65537",
+    "150_50_0_Value_65537",
+    "151_50_0_Value_65537",
+    "152_50_0_Value_65537",
+    "153_50_0_Value_65537",
+    "154_50_0_Value_65537",
+    "155_50_0_Value_65537",
+    "156_50_0_Value_65537",
+    "157_50_0_Value_65537",
+    "158_50_0_Value_65537",
+    "159_50_0_Value_65537",
+    "161_50_0_Value_65537",
+    "162_50_0_Value_65537",
+    "163_50_0_Value_65537",
+    "164_50_0_Value_65537",
+    "166_50_0_Value_65537",
+    "167_50_0_Value_65537",
+    "168_50_0_Value_65537",
+    "170_50_0_Value_65537",
+    "171_50_0_Value_65537",
+    "172_50_0_Value_65537",
+    "173_50_0_Value_65537",
+    "175_50_0_Value_65537",
+    "176_50_0_Value_65537",
+    "177_50_0_Value_65537"
+]
+
+# lisääntyvä arvo
+heater_id = [
+    "47_50_1_Value_65537"
+]
+
+# en saanut arvoja
+solar_prod_ids = [
+    "yield-today",
+    "yieldyesterday",
+    "yieldsincereset"
+]
+
+# en saanut arvoja
+inverter_prod_id = [
+    "energy"
+]
+
+# en saanut arvoja
+wind_prod_id = [
+    "produced_energy"
+]
+
+# en saanut arvoja
+phase3_prod_ids = [
+    "189_50_1_Value_65538",
+    "121_50_1_Value_65538"
+]
